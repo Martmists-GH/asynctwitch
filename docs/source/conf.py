f5f1bd9b371c88ddefdf37c7572db0f414462907
martmists <martmists@gmail.com>
2017-03-07 17:56:18 +0100

    Fix config for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,10 +37,7 @@ extensions = [
     'sphinx.ext.extlinks',
 ]
 
-if on_rtd:
-    extensions.append('sphinxcontrib.napoleon')
-else:
-    extensions.append('sphinx.ext.napoleon')
+extensions.append('sphinx.ext.napoleon')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
