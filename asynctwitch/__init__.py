f7bb9229543429a817e535262431822755fa15a5
Martmists <martmists@gmail.com>
2020-02-02 00:06:04 +0100

    Update test

---
+++ asynctwitch/__init__.py
@@ -0,0 +1,4 @@
+from asynctwitch.bots import BotBase, ChatLogBot
+from asynctwitch.entities import Badge, Emote, Message, Object, User
+
+__all__ = "BotBase", "ChatLogBot", "Badge", "Emote", "Message", "Object", "User"
--- run_test.py
+++ run_test.py
@@ -1,32 +1,5 @@
 import asynctwitch as at
 
-
-class Bot(at.CommandBot, at.RankedBot):
-	pass
-
-
-bot = Bot(
-    user='justinfan100'  # read-only client
-)
-
-
-@bot.command("test", desc="Some test command")
-async def test(m, arg1:int):
-	pass
-
-
-bot.add_rank("test rank", points=10)
-
-
-@bot.override
-async def raw_event(data):
-    print(data)
-
-
-@bot.override
-async def event_roomstate(channel, tags):
-    bot.stop(exit=True)
-    print('Failed to exit!')
-
+bot = at.ChatLogBot(channels=["thimen24"])
 
 bot.start()
